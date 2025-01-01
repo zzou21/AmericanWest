a565ba608db02f448366ead544e2e34a6bf047cb
Jerry Zou <zhihui.zou@duke.edu>
2025-01-01 12:59:29 -0500

    caret rotate

--- currentSemester/scheduleDropdown.js
+++ currentSemester/scheduleDropdown.js
@@ -4,6 +4,9 @@ scheduleDropdownButtons.forEach(button => {
     button.addEventListener("click", () => {
         const dropdownID = button.getAttribute("data-target");
         const dropdownContent = document.getElementById(dropdownID);
-        dropdownContent.classList.toggle("active")
+        dropdownContent.classList.toggle("active");
+
+        const rotateCaret = button.querySelector(".caret")
+        rotateCaret.classList.toggle("rotate")
     })
 })
